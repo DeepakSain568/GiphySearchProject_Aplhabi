d1abfc29b62cfb517d38cef9900da24dc1fec56b
Deepak Sain <89716461+DeepakSain568@users.noreply.github.com>
2023-12-21 17:14:06 +0530

    Add files via upload
    this is my AlphaBi Project

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAy64C6yJqNa4pQq1PGlboeZL_tplHchG8",
+    authDomain: "alphabiproject.firebaseapp.com",
+    projectId: "alphabiproject",
+    storageBucket: "alphabiproject.appspot.com",
+    messagingSenderId: "995102611694",
+    appId: "1:995102611694:web:9c5f55c30896291b197bff",
+    measurementId: "G-6TPZTLGZ2C"
+  };
+
+  export default firebaseConfig;
